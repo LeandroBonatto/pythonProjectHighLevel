25525eb4546409b0188b51426bc04d630169f13f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-19 15:29:21 -0500

    submit

---
+++ week02_elementary_programming/compute_area.py
@@ -0,0 +1,11 @@
+# Assign a radius
+import math
+
+radius = 20  # radius is now 20
+
+# Compute area
+area = radius * radius * 3.14159
+area = radius * radius * math.pi
+
+# Display results
+print("The area for the circle of radius", radius, "is", area)
