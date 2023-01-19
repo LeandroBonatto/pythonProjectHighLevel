03fe1d6e60394fbc5c489e047d398d15ca26e972
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2023-01-19 11:52:11 -0500

    Initial commit

---
+++ Week02/calcArea.py
@@ -0,0 +1,15 @@
+# define radius value
+import math
+
+radius = 10
+# radius = float(input('Please enter a radius: '))
+radius = eval(input('Please enter a radius: '))
+
+# calculate area of the circle
+area = radius * radius * 3.14
+area = radius * radius * math.pi
+
+# printing the area
+print('The area of the circle with radius', radius, 'is', area)
+# print('The area of the circle is' + str(area)) - + menas concatenation
+
---
+++ Week1_Cont/test.py
@@ -0,0 +1,10 @@
+"""
+Date: Jan 13, 2013
+Name: Leo Mont
+"""
+
+message1 = "Welcome to the class"
+print(message1)
+
+message2 = 'Python is fun!'
+print(message2)
